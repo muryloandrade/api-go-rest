f5aa8eae11a1a5ebd669d8c54e87eb60b139ee26
Murylo Andrade Silva <muryloandrades@gmail.com>
2023-06-30 11:54:53 -0300

    fazendo rota home

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bem vindo a rota Home")
+
+}
+
+func HandleResquests() {
+	http.HandleFunc("/", Home)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
 func main() {
-	println("Hello World")
+	fmt.Println("Iniciando servidor, redirecionando para home...")
+	HandleResquests()
 }
